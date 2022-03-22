0e960c45d1420642c2741f8c4d57288298c7e8f1
Krishan <krishanhewitt@hotmail.com>
2022-03-22 08:58:17 -0700

    Revised 'app.js' to 'server.js', 'msg' to 'message'

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -8,31 +8,31 @@ const Trip = require('../../models/Trip');
 router.get('/:id', (req, res) => {
     Trip.findById(req.params.id)
         .then(trips => res.status(200).json(trips))
-        .catch(err => res.status(404).json({ msg: 'No Trips Found - Error: ' + err }));        
+        .catch(err => res.status(404).json({ message: 'No Trips Found - Error: ' + err }));        
 });
 
 //@route POST api/trips
 //@description Add/Save trip
 router.post('/', (req, res) => {
     Trip.create(req.body)
-        .then(trips => res.status(200).json({ msg: 'Trip added successfully' })) //change to 201
-        .catch(err => res.status(400).json({ msg: 'Trip failed to add -  - Error: ' + err }));        
+        .then(trips => res.status(200).json({ message: 'Trip added successfully' })) //change to 201
+        .catch(err => res.status(400).json({ message: 'Trip failed to add -  - Error: ' + err }));        
 });
 
 //@route PUT api/trips/:id
 //@description Update trip
 router.put('/:id', (req, res) => {
     Trip.findByIdAndUpdate(req.params.id, req.body)
-        .then(trip => res.status(200).json({ msg: 'Trip updated successfully' }))
-        .catch(err => res.status(400).json({ msg: 'Unable to update the Database - Error: ' + err }));        
+        .then(trip => res.status(200).json({ message: 'Trip updated successfully' }))
+        .catch(err => res.status(400).json({ message: 'Unable to update the Database - Error: ' + err }));        
 });
 
 //@route DELETE api/trips/:id
 //@description Delete trip by id
 router.delete('/:id', (req, res) => {
     Trip.findByIdAndRemove(req.params.id, req.body)
-        .then(trips => res.status(200).json({ msg: 'Trip deleted successfully' }))
-        .catch(err => res.status(404).json({ msg: 'No Trips Found - Error: ' + err }));        
+        .then(trips => res.status(200).json({ message: 'Trip deleted successfully' }))
+        .catch(err => res.status(404).json({ message: 'No Trips Found - Error: ' + err }));        
 });
 
 module.exports = router;
