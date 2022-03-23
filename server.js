56680be725890eba4f13710437ed1c84acfeb269
Krishan <krishanhewitt@hotmail.com>
2022-03-23 09:01:46 -0700

    Installed bcrypt npm package

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use(cors({ origin: true, credentials: true }));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = require('./config/').get('jwt');
-
-
 
 //Trip routes
 app.use('/api/trips', require('./routes/api/trips'));
